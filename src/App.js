529cdecd567c0c23618a827c0c7e522cc900f517
Nico Tambellini <ntambellini13@gmail.com>
2020-05-30 10:18:32 -0600

    update men-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage.component';
 import './pages/homepage.styles.scss';
 
-function App() {
-  return <div>
-    <HomePage />
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
   </div>
+)
+
+function App() {
+  return (
+  <div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats' component={HatsPage} />
+    </Switch>
+  </div>
+  );
 }
 
 export default App;
