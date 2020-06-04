aa679cfec8366e9207463ce9ecd315f70775393f
Nico Tambellini <ntambellini13@gmail.com>
2020-06-04 07:17:48 -0600

    added sign-in component and pieces

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
-import './pages//homepage/homepage.styles.scss';
-
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import ShopPage from './pages/shop/shop.component';
 
+
+import './pages//homepage/homepage.styles.scss';
 
 function App() {
   return (
   <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop' component={ShopPage} />
+      <Route exact path='/signin' component={SignInAndSignUp} />
     </Switch>
   </div>
   );
